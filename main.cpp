d237448c57190cf0a44e960bf7eec23113caf649
YanOScompany <56045160+YanOScompany@users.noreply.github.com>
2020-12-24 07:38:34 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 string letters = "abcdefghijklmnopqrstuvwxyz";
 vector<char> alphabet;
-int key;
+int key, option;
 string word;
 string result = "";
 
@@ -16,8 +16,12 @@ int main (){
    << "     }---------------------------------{\n" 
    << "}-------------\033[32m CAESAR ENCRYPTOR X\033[31m --------------{\n"
    << "     }---------------------------------{"       
-   << "\n           Made By Trippingcarpet"     
+   << "\n           Made By Trippingcarpet\n\n"     
    <<  "\033[0m\n";
+  cout << "ENTER OPTION\n";
+  cout << "[1]Encrypt Word\n";
+  cout << "[2]Decrypt Word\n";
+  cout << "[~]Enter option: ";cin>>option;cout << endl;
   cout << "[~]Enter WORD & Key: ";
   cin >> word >> key;
   cout << "\n";
@@ -37,13 +41,27 @@ int main (){
       exit(0);
     }
   }
-
+  if(option == 1)
+  {
   for(char z : word){
     for(int i = 0; i < alphabet.size();i++){
-      if(tolower(z) == alphabet[i]){
-        result += alphabet[i+key];
+        if(tolower(z) == alphabet[i]){
+          result += alphabet[i+key];
+        }
       }
     }
+  }
+  else if(option == 2){
+  for(char z : word){
+    for(int i = 0; i < alphabet.size();i++){
+        if(tolower(z) == alphabet[i]){
+          result += alphabet[i-key];
+        }
+      }
+    }    
+  }
+  else{
+    cout << "\033[1;31m[-]NO OPTION\033[0m\n";
   }
   cout << "\033[32m[+]Output: " << result << "\033[32m" << endl;
   return 0;
